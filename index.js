fab0b25ddd1197331b8089cfa55747bc0422e727
Jordan Harband <ljharb@gmail.com>
2013-08-28 20:20:36 -0700

    Implementation.

---
+++ index.js
@@ -0,0 +1,23 @@
+/*jslint node: true */
+
+module.exports = (function () {
+	"use strict";
+
+	var toStr = Object.prototype.toString;
+	var dateClass = '[object Date]';
+
+	return function daytime(day, time) {
+		if (toStr.call(day) !== dateClass || toStr.call(time) !== dateClass) {
+			throw new TypeError('both day and time must be Date objects');
+		}
+
+		return new Date(
+			day.getFullYear(),
+			day.getMonth(),
+			day.getDate(),
+			time.getHours(),
+			time.getMinutes()
+		);
+	};
+}());
+
